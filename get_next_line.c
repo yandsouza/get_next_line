407bc1d1edc1766fdcce685b9a217c101bc3b4f9
Yann D'Souza <yannssouza@outlook.com>
2026-06-13 13:55:48 -0300

    Leaks verification

--- get_next_line.c
+++ get_next_line.c
@@ -23,7 +23,7 @@ char	*allocate_line(int fd, char *stash)
 	while (!ft_strchr(stash, '\n'))
 	{
 		buf_size = read(fd, buffer, BUFFER_SIZE);
-		if (buf_size == 0)
+		if (buf_size <= 0)
 		{
 			free(buffer);
 			free(stash);
@@ -31,11 +31,6 @@ char	*allocate_line(int fd, char *stash)
 		}
 		buffer[buf_size] = '\0';
 		stash = ft_strjoin(stash, buffer);
-		if (!stash)
-		{
-			free(buffer);
-			return (0);
-		}
 	}
 	free(buffer);
 	return (stash);
@@ -48,6 +43,8 @@ char	*next_line(char *stash)
 
 	i = 0;
 	next = malloc(sizeof(char) * (ft_strlen(stash) + 2));
+	if (!next)
+		return (0);
 	while (stash[i] != '\0' && stash[i] != '\n')
 	{
 		next[i] = stash[i];
@@ -70,6 +67,8 @@ char	*trim_line(char *stash)
 		i++;
 	i++;
 	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!new_stash)
+		return (0);
 	e = 0;
 	while (stash[i] != '\0')
 		new_stash[e++] = stash[i++];
@@ -83,6 +82,8 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	*stash;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
 	stash = allocate_line(fd, stash);
 	if (!stash)
 		return (0);
@@ -90,21 +91,21 @@ char	*get_next_line(int fd)
 	stash = trim_line(stash);
 	return (line);
 }
-//#include <stdio.h>
-//#include <fcntl.h>
-//int	main(int argc, char **argv)
-//{
-//	if (argc > 1)
-//	{
-//		int fd = open(argv[1], O_RDONLY);
-//		char	*line;
-//		while ((line = get_next_line(fd)))
-//		{
-//			if (!line)
-//				break;
-//			printf("%s", line);
-//			free(line);
-//		}
-//	}
-//	return (0);
-//}
+/*#include <stdio.h>
+#include <fcntl.h>
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		int fd = open(argv[1], O_RDONLY);
+		char	*line;
+		while ((line = get_next_line(fd)))
+		{
+			if (!line)
+				break;
+			printf("%s", line);
+			free(line);
+		}
+	}
+	return (0);
+}*/
